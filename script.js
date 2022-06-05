0c03320b55836df105cfe502d2656579e5e35377
Etcherla Sai Manoj <esmanoj.95@gmail.com>
2022-06-06 00:25:03 +0530

    Add event listeners to buttons

--- script.js
+++ script.js
@@ -14,28 +14,75 @@ function divide(a, b){
     return a / b;
 }
 
-function operate(operator, a, b){
+function mod(a, b){
+    return a % b;
+}
+
+function operate(a, operator, b){
+    a = parseFloat(a);
+    b = parseFloat(b);
     switch (operator) {
         case "+":
-            console.log(add(a, b));
+            ans = add(a, b);
             break;
         case "-":
-            console.log(subtract(a, b));
+            ans = subtract(a, b);
             break;
-        case "*":
-            console.log(multiply(a, b));
+        case "×":
+            ans = multiply(a, b);
             break;
-        case "/":
-            dconsole.log(divide(a, b));
+        case "÷":
+            ans = divide(a, b);
             break;
+        case "%":
+            ans = mod(a, b);
         default:
             console.log("Invalid operator");
             break;
     }
+    return ans;
 }
 
-// for checking correct buttons are selected
-const operators = document.querySelectorAll(".number");
-operators.forEach((op) => {
-    op.style.backgroundColor = "white";
-})
+function textAreaUpdate(number){
+    const answer = document.querySelector(".answer");
+    let value = answer.textContent;
+    value += number;
+    answer.textContent = value;
+}
+
+function historyTextUpdate(value, operator){
+    const history = document.querySelector(".count");
+    history.textContent = value + " " + operator;
+}
+
+/*******************************/
+
+let oldValue = 0;
+let inputOperator;
+let newValue = 0;
+
+const answer = document.querySelector(".answer");
+
+
+const numbers = document.querySelectorAll(".number");
+numbers.forEach((number) => {
+    number.addEventListener('click', () => {
+        textAreaUpdate(number.textContent);
+    });
+});
+
+const operators = document.querySelectorAll(".operator");
+operators.forEach((operator) => {
+    operator.addEventListener('click', () => {
+        oldValue = answer.textContent;
+        inputOperator = operator.textContent;
+        answer.textContent = '';
+    });
+});
+
+const equal = document.querySelector(".equal");
+equal.addEventListener('click', () => {
+    newValue = answer.textContent;
+    console.log(oldValue + " " + inputOperator + " " + newValue);
+    answer.textContent = operate(oldValue, inputOperator, newValue);
+});
